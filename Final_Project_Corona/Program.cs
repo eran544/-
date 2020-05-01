79b957515c77c54a6585f57d14ce8afc2a45f148
Eran <eran197@gmail.com>
2020-05-02 01:05:46 +0300

    Added test for Students to check that their work will compile under our testing

--- Final_Project_Corona/Program.cs
+++ Final_Project_Corona/Program.cs
@@ -12,29 +12,100 @@ namespace Final_Project_Corona
          * Remember: No compilation error should be at the end
          * GOOD LUCK!!!
          */
-         private static void printTriplates(double[] arr)
-        {
 
-            for (int i = 0; i < arr.Length-2; i++)
-            {
-                for (int j = i+1; j < arr.Length-1; j++)
-                {
-                    for (int k = j+1; k < arr.Length; k++)
-                    {
-                        double sum = arr[i] + arr[j] + arr[k];
-                        Console.WriteLine("i= " + i + " j= "+j + " k=" + k + " sum="+sum);
-                        
-                    }
-                }
-            }
+        static void CheckYourselfForCompilationErrors()
+        {
+            //Q1
+            int[] arr = new int[62];
+            Q1(arr);
+            //Q2
+            bool[] arr2 = new bool[0];
+            Q2(arr2);
+            //Q3a - check if constructor and gets work properly
+            Sofa stam = new Sofa("Stam model", "Stan country", 0);
+            string st = stam.GetModel();
+            st = stam.GetCountry();
+            double num = stam.GetPrice();
+            //Q3b - check if you can send proparly
+            Sofa[] stamArr = { stam, stam, stam };
+            Sofa[] stamResult = ThreeSofas(stamArr, 0);
+
+            //Q4
+            HandWeight handWeight = new HandWeight();
+            bool stamBool = handWeight.IsBalanced();
+            HandWeight handWeight1 = new HandWeight(3);
+            HandWeight.MoveWeight(handWeight, handWeight1);
+            HandWeight handWeight2 = new HandWeight(3.14, 3);
+            st = handWeight.ToString();
+
+            //Q5
+            Node<int> stamNode = new Node<int>(1);
+            stamNode.SetNext(new Node<int>(2));
+            TwinList twinList = new TwinList(stamNode);
+            Node<int> stamNode1 = twinList.GetOdd();
+            Node<int> stamNode2 = twinList.GetEven();
+            Node<int> stamNode3 = twinList.SwitchChain();
+
+            //Q6
+            int stamNum = StringToInt("15");
+            stamNum = CalcStr("1", "1", '+');
+            Stack<char> stamStack = new Stack<char>();
+            stamStack.Push('=');
+            stamStack.Push('1');
+            stamStack.Push('+');
+            stamStack.Push('1');
+            stamNum = MathStack(stamStack);
+
+            //Q7
+            BinNode<int> stamTree = new BinNode<int>(1);
+            stamBool = IsStatic(stamTree);
+            stamBool = IsVeryStatic(stamTree);
+
+            //Q8
+            //car
+            Car stamCar = new Car(123, 5, 4, "Me", 5);
+            stamNum = stamCar.GetLicensePlate();
+            stamNum = stamCar.GetNumOfSeats();
+            stamNum = stamCar.GetNumOfWheels();
+            stamCar.SellVehicle("You");
+            st = stamCar.GetOwner();
+            stamCar.SetAmount(2);
+            num = stamCar.GetAmount();
+            num = stamCar.GetCapacity();
+            stamCar.FuelUp();
+            st = stamCar.ToString();
+
+            //Bicycle
+            Bicycle bicycle = new Bicycle(123, 1, 2, "Me");
+            stamNum = bicycle.GetLicensePlate();
+            stamNum = bicycle.GetNumOfSeats();
+            stamNum = bicycle.GetNumOfWheels();
+            bicycle.SellVehicle("You");
+            st = bicycle.GetOwner();
+            bicycle.Ride(5);
+            bicycle.Charge(5);
+            st = stamCar.ToString();
+
+            //Bus
+            Bus bus = new Bus(123, 50, 10, "Dan Beersheva", 100, 8);
+            stamNum = bus.GetLicensePlate();
+            stamNum = bus.GetNumOfSeats();
+            stamNum = bus.GetNumOfWheels();
+            bus.SellVehicle("Metropoline");
+            st = bus.GetOwner();
+            bus.SetAmount(2);
+            num = bus.GetAmount();
+            num = bus.GetCapacity();
+            bus.FuelUp();
+            st = bus.ToString();
+            stamNum = bus.GetBusLine();
+            bus.ChangeBusLine(25);
+
         }
 
         static void Main()
         {
             TestTheAssignment.StartTest();
-            //double[] arr = { 2062.12, 9091.98,  5284.59, 1166.33, 4312.06, 1975.91, 7571.80, 6110.4};
-            //printTriplates(arr);
-
         }
 
         public static int Q1(int[] arr)
@@ -47,7 +118,7 @@ namespace Final_Project_Corona
                 if (arr[i] > 669)
                     count++;
             }
-            Console.WriteLine("sum of 3 digits numbers: " + sum);
+            Console.WriteLine("Sum of 3 digits numbers: " + sum);
             return count;
         }
 

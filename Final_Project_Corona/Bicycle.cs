7ebf62a2691cd2fdc5b0d0fcf5e49728aaa879d4
Eran <eran197@gmail.com>
2020-05-05 23:33:38 +0300

    Fixed solution + added GetCurrentPrecentage

--- Final_Project_Corona/Bicycle.cs
+++ Final_Project_Corona/Bicycle.cs
@@ -8,13 +8,18 @@ namespace Final_Project_Corona
     {
         private const double MAX_PRECENTAGE = 100;
         private const int PERCENTAGE_PER_KM = 5;
-        private const double PERCENTAGE_PER_MINUTES = 0.5;
+        private const double PERCENTAGE_PER_MINUTES = 3;
         private double currentPrecentage;
 
         public Bicycle(int licensePlate, int numOfSeats, int numOfWheels, string owner) :
             base (licensePlate, numOfSeats, numOfWheels, owner)
         {
             currentPrecentage = 50;
+        }
+
+        public double GetCurrentPrecentage()
+        {
+            return currentPrecentage;
         }
 
         public void Ride(int km)
